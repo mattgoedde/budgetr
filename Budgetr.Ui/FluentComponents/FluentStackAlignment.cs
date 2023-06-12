6022e5f0890a90d16862257d2e933ddde0c1b9b2
Matthew Goedde <MRGoedde@marathonpetroleum.com>
2023-06-12 15:10:07 -0400

    add fluent components

---
+++ Budgetr.Ui/FluentComponents/FluentStackAlignment.cs
@@ -0,0 +1,42 @@
+
+/// <summary>
+/// Determines the horizontal alignment of the content within the <see cref="Stack"/>.
+/// </summary>
+public enum FluentStackHorizontalAlignment
+{
+    /// <summary>
+    /// The content is aligned to the left.
+    /// </summary>
+    Left,
+
+    /// <summary>
+    /// The content is center aligned.
+    /// </summary>
+    Center,
+
+    /// <summary>
+    /// The content is aligned to the right.
+    /// </summary>
+    Right,
+}
+
+/// <summary>
+/// Determines the vertical alignment of the content within the <see cref="Stack"/>.
+/// </summary>
+public enum FluentStackVerticalAlignment
+{
+    /// <summary>
+    /// The content is aligned to the top.
+    /// </summary>
+    Top,
+
+    /// <summary>
+    /// The content is center aligned.
+    /// </summary>
+    Center,
+
+    /// <summary>
+    /// The content is aligned to the bottom
+    /// </summary>
+    Bottom,
+}
