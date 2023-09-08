dfd192fe693e64b8761fd2468f06e7551e1cf2e1
Matthew Goedde <MRGoedde@marathonpetroleum.com>
2023-09-08 13:12:28 -0400

    move to isolated process

---
+++ Budgetr.Api/HealthCheck.cs
@@ -0,0 +1,27 @@
+using System.Net;
+using Budgetr.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Budgetr.Api;
+
+public class HealthCheck
+{
+    private readonly ILogger<HealthCheck> _logger;
+
+    public HealthCheck(ILogger<HealthCheck> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function(nameof(HealthCheck))]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+    {
+        using var timer = new MethodTimeLogger<HealthCheck>(_logger);
+
+        return new OkResult();
+    }
+}
