a61eac2841c9a4a1bf64833e03d6706871e309a6
Matthew Goedde <MRGoedde@marathonpetroleum.com>
2023-09-08 09:29:42 -0400

    move into src folder

---
+++ src/Budgetr.Logic/Extensions/AsyncLinqExtensions.cs
@@ -0,0 +1,38 @@
+﻿namespace Budgetr.Logic.Extensions;
+
+public static class AsyncLinqExtensions
+{
+    /// <summary>
+    /// True if all Func return True
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="source"></param>
+    /// <param name="func"></param>
+    /// <returns>True if all Func return True</returns>
+    public static async Task<bool> AllAsync<T>(this IEnumerable<T> source, Func<T, Task<bool>> func)
+    {
+        foreach(var element in source)
+        {
+            if (!await func(element))
+                return false;
+        }
+        return true;
+    }
+
+    /// <summary>
+    /// True if any Func returns True
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="source"></param>
+    /// <param name="func"></param>
+    /// <returns>True if any Func returns True</returns>
+    public static async Task<bool> AnyAsync<T>(this IEnumerable<T> source, Func<T, Task<bool>> func)
+    {
+        foreach(var element in source)
+        {
+            if (await func(element))
+                return true;
+        }
+        return false;
+    }
+}
---
+++ src/Budgetr.Logic/Extensions/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Budgetr.Logic.Extensions;
+
+public static class StringExtensions
+{
+    public static bool IsValidUrl(this string str)
+    {
+        try
+        {
+            if (string.IsNullOrWhiteSpace(str)) return false;
+            _ = new Uri(str);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
