012f779046d17433317099bb6d60f3bf1c9fbde3
Matthew Goedde <MRGoedde@marathonpetroleum.com>
2023-09-08 15:59:57 -0400

    fix route and add nugetconfig

--- src/Budgetr.Api/HealthCheck.cs
+++ src/Budgetr.Api/HealthCheck.cs
@@ -1,9 +1,7 @@
-using System.Net;
 using Budgetr.Shared;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
-using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
 
 namespace Budgetr.Api;
@@ -18,7 +16,7 @@ public class HealthCheck
     }
 
     [Function(nameof(HealthCheck))]
-    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = "api/HealtCheck")] HttpRequest req)
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req)
     {
         using var timer = new MethodTimeLogger<HealthCheck>(_logger);
 
