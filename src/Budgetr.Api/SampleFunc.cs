52b3f7d05671a14b79078af4dc900324ce80d205
Matthew Goedde <MRGoedde@marathonpetroleum.com>
2023-09-13 13:44:52 -0400

    fix build errors

--- src/Budgetr.Api/SampleFunc.cs
+++ src/Budgetr.Api/SampleFunc.cs
@@ -1,8 +1,10 @@
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
+using Budgetr.Class;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+using System.Net;
 
 namespace Budgetr.Api;
 
