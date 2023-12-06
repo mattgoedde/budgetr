ae922250acdaaa839bd0cdf2c84c25cec22151b1
Matthew Goedde <MRGoedde@marathonpetroleum.com>
2023-12-06 11:04:37 -0500

    add db creation scripts

---
+++ src/Budgetr.DataAccess/Scripts/1_EnumConversions.sql
@@ -0,0 +1,13 @@
+﻿BEGIN TRANSACTION;
+GO
+
+ALTER TABLE [AmortizedLoans] ADD [LoanType] int NOT NULL DEFAULT 0;
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20230917182249_EnumConversions', N'8.0.0');
+GO
+
+COMMIT;
+GO
+
